e26719560622777c367f3aaf82a7394dc827cb8d
SaltyMemeBoy <mr.saltyboy@gmail.com>
2025-02-13 12:17:32 -0600

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://open.spotify.com/embed/track/2d8JP84HNLKhmd6IYOoupQ?utm_source=generator", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/duv3anwg2/video/upload/v1739470536/pi2hsshit3byc0gyrdlj.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
